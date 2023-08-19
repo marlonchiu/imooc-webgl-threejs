61e96c046fe6d172e8ca05a857db76d1c7340f70
marlonchiu <Knight1898@outlook.com>
2023-08-19 22:29:37 +0800

    17-7 给建筑物添加渐变色

---
+++ city-real-time/src/config/index.js
@@ -0,0 +1,6 @@
+export const color = {
+    mesh: '#1B3045', // 建筑物颜色
+    head: '#ffffff', // 建筑物顶部颜色
+    soundLine: '#4f90bb', // 建筑物外围的线条颜色
+    risingColor: '#5588AA', // 建筑物扫描线条颜色
+}
--- city-real-time/src/effect/surroundLine.js
+++ city-real-time/src/effect/surroundLine.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { color } from '../config/index.js'
 
 export class SurroundLine {
     constructor(scene, child) {
@@ -8,28 +9,56 @@ export class SurroundLine {
         this.render()
     }
 
+    computedMesh() {
+        this.child.geometry.computeBoundingBox();
+        this.child.geometry.computeBoundingSphere();
+    }
+
     render() {
+        this.computedMesh()
+        const {max, min} = this.child.geometry.boundingBox
+
+        // 高度差
+        const size = max.z - min.z;
+
         const material = new THREE.ShaderMaterial({
             uniforms: {
                 u_city_color: {
                     // 得需要一个模型颜色 最底部显示的颜色
-                    value: new THREE.Color('#1B3045')
+                    value: new THREE.Color(color.mesh)
                 },
-
+                u_head_color: {
+                    // 要有一个头部颜色 最顶部显示的颜色
+                    value: new THREE.Color(color.head)
+                },
+                u_size: {
+                    value: size,
+                },
             },
             vertexShader: `
+                varying vec3 v_position;
+            
                 void main() {
+                    v_position = position;
                     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
                 }
             `,
             fragmentShader: `
+                varying vec3 v_position;
+        
                 uniform vec3 u_city_color;
+                uniform vec3 u_head_color;
+                uniform float u_size;
                 
                 void main() {
-                    gl_FragColor = vec4(u_city_color, 1.0);
+                    vec3 base_color = u_city_color;
+                    base_color = mix(base_color, u_head_color, v_position.z / u_size);
+                    
+                    gl_FragColor = vec4(base_color, 1.0);
                 }
             `,
         })
+        
         const mesh = new THREE.Mesh(this.child.geometry, material);
 
         // 让mesh 继承 child 的旋转、缩放、平移
